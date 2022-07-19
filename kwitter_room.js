9e3877a4b7e2eeadc0a8012ca52eb6b5f363e712
Tanisha-2010 <104196498+Tanisha-2010@users.noreply.github.com>
2022-07-19 21:17:08 +0530

    Add files via upload
    updated firebase

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgiMjmUUVhFPYzf5SFl2vDCwyD1dpyon4",
+    authDomain: "kwitterproject-ae1db.firebaseapp.com",
+    databaseURL: "https://kwitterproject-ae1db-default-rtdb.firebaseio.com",
+    projectId: "kwitterproject-ae1db",
+    storageBucket: "kwitterproject-ae1db.appspot.com",
+    messagingSenderId: "22698403051",
+    appId: "1:22698403051:web:2bf6a86d41fc9a3fca8aae"
+};
+
+// Initialize Firebase
+firebaseConfig.initializeApp(firebaseConfig);
